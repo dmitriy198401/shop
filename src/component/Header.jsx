95ff7d90ed17cc5b2b7ae34e4a1a61ff9c5c4369
dmitriy198401 <160256606+dmitriy198401@users.noreply.github.com>
2024-02-28 20:54:50 +0400

    Clear project directory and
    Clear project

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+            <span className="Logo">Phone Sell</span>
+            </div>
+            <div className="presentation"></div>
+
+        </header>
+    );
+}
